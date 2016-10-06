b0876b7775fbe21cc04351fec781a93dfa5b5f57
Shou Ya <github@lain.li>
2016-10-06 21:36:00 +0800

    linux compiling script

---
+++ compile-linux-x64.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+STACK_OPTS='--allow-different-user'
+
+docker build -t poi-linux-x64 .
+
+docker run -it --rm \
+       -v `pwd`:/tmp/poi \
+       -v $HOME/.stack:/root/.stack \
+       -w /tmp/poi \
+       poi-linux-x64 \
+       sh -c "stack ${STACK_OPTS} setup && stack ${STACK_OPTS} build"
